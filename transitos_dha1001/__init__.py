0a4de76b29c1006f10f9b05e81845a671c6c6166
rjassef <roberto.assef@mail.udp.cl>
2021-04-12 16:31:48 -0400

    Agrega rutinas de flat y reduccion de ciencia a init.

--- transitos_dha1001/__init__.py
+++ transitos_dha1001/__init__.py
@@ -3,3 +3,5 @@ name = "transitos_dha1001"
 #Importar todos los modulos de reduccion.
 from .reduccion.master_bias import crear_masterbias
 from .reduccion.master_dark import crear_masterdark
+from .reduccion.master_dark import crear_masterflat
+from .reduccion.science import reducir_imagenes_ciencia
