d8325a316eff41b6f3b50582b2d36bae33e2a87f
rjassef <roberto.assef@mail.udp.cl>
2021-04-12 16:32:13 -0400

    Agrega rutinas de reduccion de imagenes de ciencia.

---
+++ transitos_dha1001/reduccion/science.py
@@ -0,0 +1,74 @@
+import numpy as np
+from astropy.io import fits
+from astroscrappy import detect_cosmics
+
+def reducir_imagenes_ciencia(imagenes, prefijo="ciencia",
+                     reyeccion_rayos_cosmicos=True,
+                     nombre_flat="MasterFlat.fits",
+                     nombre_dark="MasterDark.fits",
+                     nombre_bias="MasterBias.fits",
+                     directorio_entrada="raw", directorio_salida="red"):
+    """
+    Rutina para reducir las imagenes de ciencia. Esta rutina sustrae el bias y dark, y corrige las diferencias de sensibilidad entre pixeles usando el flat en imagenes tomadas por la camara del telescopio MAS de 50cm en El Sauce.
+
+    Parámetros
+    ----------
+
+    imagenes: lista
+        Lista de las imagenes que vamos a combinar.
+
+    prefijo: string, opcional
+        Prefijo que se le antepondrá al prefijo de la imagen para indicar que ha sido reducida.
+
+    reyeccion_rayos_cosmicos: boolean, opcional
+        True si se desea remover los rayos cósmicos.
+
+    nombre_flat: string, opcional
+        Nombre de la imagen que tiene el cuadro de Flat combinado.
+
+    nombre_dark: string, opcional
+        Nombre de la imagen que tiene el cuadro de dark combinado.
+
+    nombre_bias: string, opcional
+        Nombre de la imagen que tiene el cuadro de bias combinado.
+
+    directorio_entrada: string, opcional
+        Directorio donde están las imágenes tomadas por el telescopio.
+
+    directorio_salida: string, opcional
+        Directorio donde vamos a guardar las imagenes reducidas.
+
+    """
+
+    #Abrir el master dark y el master bias.
+    if nombre_bias is not None:
+        master_bias = fits.open("{}/{}".format(directorio_salida, nombre_bias))
+    if nombre_dark is not None:
+        master_dark = fits.open("{}/{}".format(directorio_salida, nombre_dark))
+    if nombre_flat is not None:
+        master_flat = fits.open("{}/{}".format(directorio_salida, nombre_flat))
+
+
+    #Pasar por cada imagen removiendo bias y dark, corrigiendo por el flat, y removiendo los rayos cósmicos.
+    for imagen in imagenes:
+
+        #Abrir la imagen.
+        fname = "{}/{}".format(directorio_entrada, imagen)
+        h = fits.open(fname)
+
+        #Sustraer el bias y el dark, y corregir por el flat.
+        h[0].data = np.float64(h[0].data)
+        if nombre_bias is not None:
+            h[0].data -= master_bias[0].data
+        if nombre_dark is not None:
+            h[0].data -= master_dark[0].data
+        if nombre_flat is not None:
+            h[0].data /= master_flat[0].data
+
+        #Limpiar los rayos cosmicos.
+        if reyeccion_rayos_cosmicos:
+            crmask, clean_image = detect_cosmics(h[0].data)
+            h[0].data = clean_image
+
+        #Guardar la imagen reducida.
+        h[0].writeto("{}/{}_{}".format(directorio_salida, prefijo, imagen), overwrite=True)
