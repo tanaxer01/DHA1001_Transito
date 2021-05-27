06178bd9f9734a75c8ed656e749a9977e2b19b58
rjassef <roberto.assef@mail.udp.cl>
2021-05-27 12:54:39 -0400

    Añade rutinas para alinear las imagenes de ciencia.

--- transitos_dha1001/__init__.py
+++ transitos_dha1001/__init__.py
@@ -6,6 +6,7 @@ from .reduccion.master_dark import crear_masterdark
 from .reduccion.master_flat import crear_masterflat
 from .reduccion.science import reducir_imagenes_ciencia
 from .reduccion.desplegar_imagenes import desplegar_imagen
+from .reduccion.alinear import alinear_imagenes_ciencia
 
 from .fotometria.dao import dao_busqueda, dao_recentrar
 from .fotometria.phot import medir_fotometria
---
+++ transitos_dha1001/reduccion/alinear.py
@@ -0,0 +1,50 @@
+import numpy as np
+import astroalign as aa
+from astrop.io import fits
+import os
+import re
+
+def alinear_imagenes_ciencia(imagenes,
+                    prefijo="ali",
+                    directorio_imagenes_reducidas="red",
+                    recalcular=True):
+
+    """
+    Rutina para alinear las imagenes de ciencia. La primera imagen de la lista siempre se va a usar como referencia para alinear el resto.
+
+    Parametros
+    ----------
+
+    imagenes: lista
+        Lista de imagenes de entrada que queremos alinear.
+
+    prefijo: string, opcional
+        Prefijo que se le antepondrá al prefijo de la imagen para indicar que ha sido alineada.
+
+    directorio_imagenes_reducidas: string, opcional
+        Directorio donde vamos a guardar las imagenes reducidas.
+
+    recalcular: bool, opcional
+        Si es True, se recalculará el alineamiento aún cuando una versión de la imagen alineada ya exista en el directorio de imagenes reducidas. 
+
+    """
+
+    #La primera imagen será utilizada como la referencia, y el resto se va a linear para calzar con esta.
+    referencia = fits.open("{}/{}".format(directorio_imagenes_reducidas, imagenes[0]))
+
+    #Pasar por cada imagen alineandola a la de referencia. Si no se pide recalcular, y la imagen ya existe, saltarse el alineamiento.
+    for imagen in imagenes_reducidas_ciencia:
+
+        #Ver si la imagen alineada ya existe.
+        imagen_salida = prefijo + "_" + imagen
+        if not recalcular and os.path.exists("{}/{}".format(directorio_imagenes_reducidas, imagen_salida)):
+            continue
+
+        #Alinear la imagen.
+        im = fits.open("{}/{}".format(directorio_imagenes_reducidas, imagen))
+        im_alineada, footprint = aa.register(im[0].data, referencia[0].data)
+
+        #Guardar la imagen alineada.
+        im[0].data = im_alineada
+        im.writeto("{}/{}".format(directorio_imagenes_reducidas, imagen_salida))
+        im.close()
